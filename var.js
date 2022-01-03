3a99c449d09068e88cd5a5a2fc231fe361a8be13
yuii88 <pim8828@gmail.com>
2022-01-03 15:57:39 +0700

    sum 3 lines above in 1 line

--- var.js
+++ var.js
@@ -1,6 +1,9 @@
-let name; // ตัวแปรไม่มีการกำหนดค่า จะ respresent undefined
-name = "var of hong"; //กำหนดข้อความลงไปเก็บใน var name
-let age = 21;
+// let name; // ตัวแปรไม่มีการกำหนดค่า จะ respresent undefined
+// name = "var of hong"; //กำหนดข้อความลงไปเก็บใน var name
+// let age = 21;
+
+//sum 3 lines above in 1 line
+let name = "var of hong", age=21;
 
 console.log(name);
 document.write(name);
